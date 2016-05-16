2bbf67d17f5a22dd556d555d4624f32957857fd6
thiagoanselmo <thiagoo.anselmoo@gmail.com>
2016-05-16 16:37:11 -0300

    Update

--- Inkjet + Laser/Etiqueta 6187/Etiqueta 6187.js
+++ Inkjet + Laser/Etiqueta 6187/Etiqueta 6187.js
@@ -1,40 +1,13 @@
-/*
-	Utilizado para gerar a estrutura das etiquetas 'aqueles quadradinhos' para podermos ver como será impresso
-	fiz isso para ajudar visualmente onde será impresso a etiqueta
-*/
-function setarEstrutura(){
-
-	for(var i=1; i<=numeroLinhas; i++){	
-
-		doc.lineJoin('round')
-			.rect(14.50, (altura * i ), largura, altura)    // Primeira coluna 14.5 pois é o inicio da margem
-			.stroke();										
-
-		doc.lineJoin('round')
-			.rect(62.00, (altura * i ), largura, altura)   // Segunda coluna + a margem mais densidade horizontal
-			.stroke();
-
-		doc.lineJoin('round')
-			.rect(109.05, (altura * i ), largura, altura)  // Terceira coluna é densidade anterior '62' + densidade horizontal '47.50'
-			.stroke();
-				
-		doc.lineJoin('round')
-			.rect(157.00, (altura * i ), largura, altura) // Quarta coluna é densidade anterior '109.05' + densidade horizontal '47.50'
-			.stroke();
-		}	    
-}
-
-
 function setarTextoEtiqueta(texto, marginEsquerda, marginSuperior){
 
-	doc.text(texto, marginEsquerda,  marginSuperior, {height:altura, width:largura} )
+	doc.text(texto, marginEsquerda,  marginSuperior, {height: altura, width:largura, align: 'center'} )
 }	    	
 
 /*
 	Adicionando os módulos necessários				
 */
 var PDFDocument = require('pdfkit');                      
-var fs 		= require('fs');			
+var fs 			= require('fs');		
 
 /*	
 	CRIAMOS O EXEMPLO DA PIMACO 6187 
@@ -47,10 +20,10 @@ var fs 		= require('fs');
 	Desnsidade vertical:  1,27 -> 12,70mm
 	Densidade horizontal: 4,75 -> 47,50mm
 
-	Altura etiqueta:    1,27   -> 12,70mm
+	Altura etiqueta: 	1,27   -> 12,70mm
 	Largura etiqueta:   4,44   -> 44,00mm
 
-	Numero colunas:     4
+	Numero colunas: 	4
 	Numero de linhas:   20
 	
 	Total 80 etiquetas
@@ -69,8 +42,8 @@ var numeroLinhas        = 20;
 var densidadeHorizontal = 47.50;
 
 // Tamanho da etiqueta (informações de acordo com Pimaco)
-var altura   = 12.70
-var largura  = 44.00;			
+var altura              = 12.70;
+var largura             = 44.40;			
 
 /*	
 	Criamos a folha de acordo com as dimenssões dadas pela Pimaco
@@ -79,55 +52,45 @@ var largura  = 44.00;
 doc = new PDFDocument({
 	size: papelCartaPimaco,
 	margins: {
-    	top: 12.70, 
-    	bottom: 17.70,
+    	top: 12.70,
+    	bottom: 12.70, 	        	
     	left: 14.50,
     	right: 14.50
  	}
-});
-	
-
-/*
-	Abaixo vamos criar um retangulo passando os parametros da configuração da etiqueta
-	abaixo a sintax do PDFKit para criação de um retangulo.
-
-	doc.lineWidth(-30); 					// Informamos a espeçura da linha do retangulo
-	.rect(eixo X, eixo Y, largura, altura) //  Informamos como  desenhar o retangulo em nosso PDF dand as cordenadas
-
-	Criamos o 'esqueleto vazio da etiqueta pimaco'
-*/
+});			
 
 // Escolhemos o tamanho da fonte
-doc.fontSize(8);
-
-// Vamos escrever nas etiquetas
-	
-	//Quero setar a estrutura das etiquetas 
-	setarEstrutura();
+doc.fontSize(6); 			
 
 	//Vamos popular com as etiquetas
 	for(var i=1; i<=numeroLinhas; i++){
 
 		/*
-			O 'i' corresponde a linha que estou imprimindo.
-	
-			14.50 + 3 -> É utilizado para centralizar o nome caso o nome não estja no centro pois '14.50' é a margem a esquerda
-			então adicionamos ele mais para 'dentro' da margem
-	
-			Fizemos isso mesma coisa com a altura adicionamos um pouco para baixo da margem.
+		O i corresponde a linha que estou imprimindo.
+
+		14.50 + 3 -> É utilizado para centralizar o nome caso o nome não estja no centro pois '14.50' é a margem a esquerda
+		então adicionamos ele mais para 'dentro' da margem
+
+		Fizemos isso mesma coisa com a altura adicionamos um pouco para baixo da margem.
 		*/
 
-		// Espaçamentos coluna 01 
-		setarTextoEtiqueta('Thiago', (14.50 + 3),  (altura * i + 3));
+		// Espaçamentos coluna 01  				
 
-		// Espaçamentos coluna 02
-		setarTextoEtiqueta('Thiago', (62.00 + 3),  (altura * i + 3));
+		if(i == 1){ 					
+			setarTextoEtiqueta('CIE 0000' + i, 5.50, altura);
+		}
 
-		// Espaçamentos coluna 03
-		setarTextoEtiqueta('Thiago', (109.05 + 3), (altura * i + 3));
+		if(i>1 && i<=6){
+			setarTextoEtiqueta('CIE 0000' + i, 5.50,  (altura * i) + (i/2) );
+		}
+		
+		if(i>6 && i<=10){
+			setarTextoEtiqueta('CIE 0000' + i, 5.50,  (altura * i) + (i/1.5));
+		}
 
-		// Espaçamentos coluna 04
-		setarTextoEtiqueta('Thiago', (157.00 + 3), (altura * i + 3)); 				
+		if(i>10 && i<=numeroLinhas){
+			setarTextoEtiqueta('CIE 0000' + i, 5.50,  (altura * i) + (i/1.7)); // maior a divisão mais para cima ele insere o texto
+		} 				
 	}
 
 /*
